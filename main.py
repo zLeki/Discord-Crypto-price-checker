3531d70c102d1aa7d13ee3f4232a526b19e380a9
zLeki <85647342+zLeki@users.noreply.github.com>
2021-06-22 20:53:10 -0400

    update
    changed user agent

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from requests import get, post
 from bs4 import BeautifulSoup
-
+import random
+userAgents = ["Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"]
 url = 'https://www.binance.com/en/trade/BTC_BUSD?type=spot'
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
+    'User-Agent': f'{random.choice(userAgents)}'
 }
 
 page = get(url, headers=headers)
